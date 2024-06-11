7999b2157795e9d1d2b7412368fdc7ac7760e968
Leopold Romanowski <leopold.romanowski@gmail.com>
2024-06-11 09:59:34 +0200

    Initial Commit

---
+++ Xcode16Check/Xcode16CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode16CheckApp.swift
+//  Xcode16Check
+//
+//  Created by Leopold Romanowski on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Xcode16CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Xcode16CheckTests/Xcode16CheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode16CheckTests.swift
+//  Xcode16CheckTests
+//
+//  Created by Leopold Romanowski on 11/06/2024.
+//
+
+import Testing
+@testable import Xcode16Check
+
+struct Xcode16CheckTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
